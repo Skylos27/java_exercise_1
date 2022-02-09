fabe6096303d3995c6aef791fe58351d6cc6de6c
Antoine Auger <antoine.auger@epita.fr>
2022-02-09 15:53:16 +0100

    Partie 2 : Launcher

---
+++ src/Launcher.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Launcher {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello " + (args.length > 0 ? args[0] : "World"));
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.next();
+        if (!command.equals("quit"))
+            System.out.println("Unknown command");
+    }
+}
