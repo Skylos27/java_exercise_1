031819f607fb3b4d3988b06583133e6c89660ad3
Antoine Auger <antoine.auger@epita.fr>
2022-02-10 11:20:19 +0100

    Partie 6 - un peu de refactoring

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,18 +1,34 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Launcher {
     public static void main(String[] args)
     {
         System.out.println("Hello " + (args.length > 0 ? args[0] : "World"));
         Scanner scanner = new Scanner(System.in);
-        String command = scanner.next();
+        String command = scanner.nextLine();
         while (!command.equals("quit"))
         {
             if (command.equals("fibo"))
             {
                 System.out.println("Entrez le terme souhaité : ");
                 int n = scanner.nextInt();
-                System.out.println(fibo(n));
+                System.out.println("Fibo(" + n + ") = " + fibo(n));
+            }
+            else if (command.equals("freq"))
+            {
+                System.out.println("Entrez le chemin du fichier à lire : ");
+                String pathstr = scanner.nextLine();
+                freq(pathstr);
             }
             else
                 System.out.println("Unknown command");
@@ -28,5 +44,35 @@ public class Launcher {
             return 1;
         else
             return fibo(n - 1) + fibo(n - 2);
+    }
+
+    private static void freq(String pathstr) {
+        try {
+            Path path = Paths.get(pathstr);
+            String text = Files.readString(path);
+            String[] words = text.split(" ");
+
+            Stream<String> wordStream = Arrays.stream(words);
+
+            Map<String, Long> WordCount = wordStream
+                    .filter(string -> !string.isBlank())
+                    .map(String::toLowerCase)
+                    .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+            Comparator<Map.Entry<String, Long>> countReversed =
+                    Map.Entry.<String, Long>comparingByValue()
+                            .reversed();
+
+            String top3 = WordCount.entrySet().stream()
+                    .sorted(countReversed)
+                    .limit(3)
+                    .map(Map.Entry::getKey)
+                    .collect(Collectors.joining(" "));
+
+            System.out.println(top3);
+
+        } catch (IOException e) {
+            System.out.println("Unreadable file: " + e);
+        }
     }
 }
